c4a34f2237ce1969e6b611db74ad261173ed3243
Steve Matthews <stevem1015@gmail.com>
2016-01-13 14:10:46 -0500

    create new Xcode project

---
+++ NYTPhotoViewer/NYTPhotoViewer.h
@@ -0,0 +1,19 @@
+//
+//  NYTPhotoViewer.h
+//  NYTPhotoViewer
+//
+//  Created by Steve Matthews on 1/13/16.
+//  Copyright © 2016 The New York Times Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NYTPhotoViewer.
+FOUNDATION_EXPORT double NYTPhotoViewerVersionNumber;
+
+//! Project version string for NYTPhotoViewer.
+FOUNDATION_EXPORT const unsigned char NYTPhotoViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NYTPhotoViewer/PublicHeader.h>
+
+

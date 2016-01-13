a3519614071a87b1e0ee9ab62e1bf9ee8a60931f
Steve Matthews <stevem1015@gmail.com>
2016-01-13 15:37:52 -0500

    create Example app

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Steve Matthews on 1/13/16.
+//  Copyright © 2016 The New York Times Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Example
+//
+//  Created by Steve Matthews on 1/13/16.
+//  Copyright © 2016 The New York Times Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

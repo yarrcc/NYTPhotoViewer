6dae1273d840b82153af55e0396013fb9838457e
Steve Matthews <stevem1015@gmail.com>
2016-01-13 15:40:28 -0500

    create Example swift app

---
+++ Example-Swift/Example-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example-Swift
+//
+//  Created by Steve Matthews on 1/13/16.
+//  Copyright © 2016 The New York Times Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
